8b93bdf0644c6a411e4fd9ee12580fa5f76d505f
Ajay Sawant <ajay_sawant@fulcrumww.com>
2019-10-16 16:33:36 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FinalProject';
+  loadedFeature = 'recipe';
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
