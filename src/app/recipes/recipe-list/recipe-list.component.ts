ac56715e8b58fed995074f643e0edcfd7a220fb5
Ajay Sawant <ajay_sawant@fulcrumww.com>
2019-10-16 18:59:32 +0530

    changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 @Component({
@@ -7,13 +7,16 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 recipes: Recipe[] = [
   new Recipe('A Test Recipe','This is a simple Test','https://images.herzindagi.info/image/2019/Oct/paneer-recipes.jpg'),
-  new Recipe('A Test Recipe','This is a simple Test','https://images.herzindagi.info/image/2019/Oct/paneer-recipes.jpg')
+  new Recipe('Another Test Recipe','This is a simple Test','https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/healthy_kids_main_image.jpg')
 ];
   constructor() { }
 
   ngOnInit() {
   }
-
+onRecipeSelected(recipe: Recipe){
+this.recipeWasSelected.emit(recipe);
+}
 }
